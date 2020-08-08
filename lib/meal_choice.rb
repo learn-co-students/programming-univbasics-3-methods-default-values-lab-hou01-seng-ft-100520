e90fac3209d72c0861adc4fb148a17add15d8ce4
David Molina <davidmolina2810@gmail.com>
2020-08-08 18:54:40 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,11 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+#method that will accept 2 vegetables and, optionally, a protein (default='tofu') and
+#out put two strings, returning One
+def meal_choice(veg1, veg2, protein='tofu')
+  puts "What a nutritious meal!"
+  phrase = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts phrase
+  phrase
+end
